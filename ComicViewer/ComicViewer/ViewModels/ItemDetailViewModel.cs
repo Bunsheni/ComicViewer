1c6eae6345652a217423e33731793ce270539e96
Bunsheni <32319288+Bunsheni@users.noreply.github.com>
2018-10-17 07:09:26 +0900

    초기 커밋.

---
+++ ComicViewer/ComicViewer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ComicViewer.Models;
+
+namespace ComicViewer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
